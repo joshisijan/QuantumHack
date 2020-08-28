62b60b058625dabaf8be9c815c320bf82e544b2a
keeran2488 <49069135+keeran2488@users.noreply.github.com>
2020-08-28 14:19:12 +0545

    updated AUTH_USER_MODEL

--- online_class/assignment/urls.py
+++ online_class/assignment/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
 from .views import *
 
-urlpatterns = [
-    path('',assignment_home, name='assignemnt_home'),
-    path('add/',assignment_add, name='assignemnt_add'),
-    path('<')
-]
+# urlpatterns = [
+#     path('',assignment_home, name='assignemnt_home'),
+#     path('add/',assignment_add, name='assignemnt_add'),
+#     path('<')
+# ]
 
 
 urlpatterns = [
     path('teachers/', include(([
-
+        path('', assignment_home, name='assignemnt_home'),
     ],'classromm'), namespace='teachers'))
 ]
--- online_class/online_class/settings.py
+++ online_class/online_class/settings.py
@@ -112,7 +112,7 @@ USE_L10N = True
 USE_TZ = True
 
 #AUTH
-AUTH_USER_MODEL = assignment.User
+AUTH_USER_MODEL = 'assignment.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
