8bf8f8d786c4a001d561e6f8453142281aac9313
sijan joshi <joshisijan96@gmail.com>
2020-08-28 15:33:16 +0545

    edited

--- online_class/assignment/urls.py
+++ online_class/assignment/urls.py
@@ -2,8 +2,12 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
+<<<<<<< Updated upstream
     path('',assignment_home, name='assignment_page'),
     path('add/',assignment_add, name='assignment_add'),
+=======
+    path('', assignment_home, name='assignment_home'),
+>>>>>>> Stashed changes
 ]
 
 
